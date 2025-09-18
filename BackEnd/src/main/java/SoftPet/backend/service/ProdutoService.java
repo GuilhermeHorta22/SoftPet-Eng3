bb39353f9681012e431537dd91f9d271ac525da0
GuilhermeHorta22 <ghorta800@gmail.com>
2025-09-18 12:03:44 -0300

    padrão observer aplicado no backEnd

--- BackEnd/src/main/java/SoftPet/backend/service/ProdutoService.java
+++ BackEnd/src/main/java/SoftPet/backend/service/ProdutoService.java
@@ -2,8 +2,13 @@ package SoftPet.backend.service;
 
 import SoftPet.backend.DAO.ProdutoDAO;
 
+import SoftPet.backend.dto.DoacaoDTO;
+import SoftPet.backend.dto.PessoaCompletoDTO;
 import SoftPet.backend.dto.ProdutoDTO;
+import SoftPet.backend.model.DoacaoModel;
+import SoftPet.backend.model.PessoaModel;
 import SoftPet.backend.model.ProdutoModel;
+import SoftPet.backend.observer.Subject;
 import SoftPet.backend.util.Validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +17,17 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ProdutoService {
+public class ProdutoService implements Subject
+{
 
     @Autowired
     private ProdutoDAO produtoDAO;
+
+    //vou usar para observar a quantidade do produto
+    private PessoaService observer;
+
+    //vou usar para ter o controle de quem eu vou mandar o email quando o estoque chegar em 0
+    private List<PessoaCompletoDTO> listaNotify;
 
     public ProdutoModel addProduto(ProdutoDTO produtoDTO) throws Exception {
         ProdutoModel produto = produtoDTO.getProduto();
@@ -89,6 +101,53 @@ public class ProdutoService {
         return produtoDAO.getProdutosPorTipo(tipo);
     }
 
+    @Override
+    public void adicionarObserver(PessoaCompletoDTO pessoa)
+    {
+        if(pessoa.getPessoa().getNotificar() == true && !listaNotify.contains(pessoa))
+            listaNotify.add(pessoa);
+    }
 
+    @Override
+    public void removerObserver(PessoaCompletoDTO pessoa)
+    {
+        if(listaNotify.contains(pessoa))
+            listaNotify.remove(pessoa);
+    }
 
+    @Override
+    public void notificarObserver(ProdutoModel produto)
+    {
+        observer = new PessoaService();
+        for(PessoaCompletoDTO pessoa : listaNotify)
+            observer.update(produto, pessoa);
+    }
+
+    public void consumirProduto(Long id, int qtdeConsumida) throws Exception
+    {
+        if(id == null || id <= 0)
+            throw new IllegalArgumentException("ID do produto inválido!");
+
+        if(!Validation.numNegativo(qtdeConsumida))
+            throw new IllegalArgumentException("Quantidade do produto inválida!");
+
+        ProdutoDTO produtoExistente = produtoDAO.findByProduto(id);
+        if(produtoExistente == null)
+            throw new Exception("Não existe uma doação com esse ID!");
+
+        int estoqueAtual = produtoExistente.getProduto().getQuantidadeEstoque();
+
+        if(qtdeConsumida > estoqueAtual)
+            throw new IllegalArgumentException("Quantidade a consumir maior que o estoque atual!");
+
+        int novoEstoque = estoqueAtual - qtdeConsumida;
+        ProdutoModel produtoAtualizado = produtoExistente.getProduto();
+        produtoAtualizado.setQuantidadeEstoque(novoEstoque);
+
+        produtoDAO.updateProduto(produtoAtualizado);
+
+        //chamada do notificar do observer
+        if(novoEstoque <= 0)
+            notificarObserver(produtoAtualizado);
+    }
 }
